d85cc740001750909dc5993c8bd074282b0df64b
fergmac <f.macconnell@gmail.com>
2017-03-27 15:03:12 -0700

    revert back to newest version of reduxForm

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password}) {
@@ -8,12 +10,16 @@ export function signinUser({ email, password}) {
 
     // Submit email/password to server
     axios.post(`${ROOT_URL}/signin`, { email, password })
-    // If request is good...
-    // - Update state to indicate user is auth'd
-    // - Save the JWT token
-    // - Redirect to route '/feature'
-
+        .then(response => {
+        // If request is good...
+        // - Update state to indicate user is auth'd
+        // - Save the JWT token
+        // - Redirect to route '/feature'
+        browserHistory.push('/feature');
+    })
+    .catch(() => {
     // If request is bad...
     // - Show an error to user
+    })
     }
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
 class Signin extends Component {
@@ -13,22 +12,22 @@ class Signin extends Component {
         // handleSubmit, email and password from reduxForm
         const { handleSubmit, fields: { email, password }} = this.props;
         return (
-            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-                <fieldset className="form-group">
-                    <label>Email:</label>
-                    <Field name="email" component="input" className="form-control" />
-                    <label>Password:</label>
-                    <Field name="password" component="input" className="form-control" />
-                </fieldset>
-                <button action="submit" className="btn btn-primary">Sign In</button>
-            </form>
+        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <fieldset className="form-group">
+            <label>Email:</label>
+            <input {...email} className="form-control" />
+            </fieldset>
+            <fieldset className="form-group">
+            <label>Password:</label>
+            <input {...password} type="password" className="form-control" />
+            </fieldset>
+            <button action="submit" className="btn btn-primary">Sign in</button>
+        </form>
         );
     }
 }
 
-Signin = reduxForm({
+export default reduxForm({
     form: 'signin',
     fields: ['email', 'password']
-})(Signin);
-
-export default connect(null, actions)(Signin);
+}, null, actions)(Signin);
