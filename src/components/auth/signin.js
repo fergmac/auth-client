3eca63959f4407eeb328e805baf4d9d7c725aef7
fergmac <f.macconnell@gmail.com>
2017-03-26 20:44:54 -0700

    add handleFormSubmit for signin

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 
 class Signin extends Component {
+    handleFormSubmit({ email, password }) {
+        console.log(email, password);
+        // Log in User
+    }
     render() {
+        // handleSubmit, email and password from reduxForm
+        const { handleSubmit, fields: { email, password }} = this.props;
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <fieldset className="form-group">
                     <label>Email:</label>
-                    <input className="form-control" />
+                    <Field name="email" component="input" className="form-control" />
                     <label>Password:</label>
-                    <input className="form-control" />
+                    <Field name="password" component="input" className="form-control" />
                 </fieldset>
                 <button action="submit" className="btn btn-primary">Sign In</button>
             </form>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRouter, browserHistory } from 'react-router';
 
 import App from './components/app';
+import Signin from './components/auth/Signin';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -13,6 +14,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory} >
       <Route path="/" component={App} />
+      <Route path="/signin" component={Signin} />
     </Router>
   </Provider>
   , document.querySelector('.container'));
