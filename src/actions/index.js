5a7817b312738e6151392d176c4686421f9f00fe
fergmac <f.macconnell@gmail.com>
2017-03-27 16:16:31 -0700

    create actions for auth-user, unauth-user, auth-error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,34 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { AUTH_USER, AUTH_ERROR } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password}) {
     return function(dispatch) {
         // redux thunk lets us return an action, which gives us direct access to dispatch
-
-
     // Submit email/password to server
     axios.post(`${ROOT_URL}/signin`, { email, password })
         .then(response => {
         // If request is good...
         // - Update state to indicate user is auth'd
+        dispatch({ type: AUTH_USER });
         // - Save the JWT token
+        localStorage.setItem('token', response.data.token );
         // - Redirect to route '/feature'
         browserHistory.push('/feature');
-    })
-    .catch(() => {
-    // If request is bad...
-    // - Show an error to user
-    })
+        })
+        .catch(() => {
+        // If request is bad...
+        // - Show an error to user
+        dispatch(authError('Bad Login Info'));
+        });
+    }
+}
+
+export function authError(error) {
+    return {
+        type: AUTH_ERROR,
+        payload: error
     }
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -8,6 +8,13 @@ class Signin extends Component {
         // Log in User
         this.props.signinUser({ email, password });
     }
+    renderAlert() {
+        if (this.props.errorMessage) {
+            <div className="alert alert-danger">
+                <strong>Oops!</strong> {this.props.errorMessage}
+            </div>
+        }
+    }
     render() {
         // handleSubmit, email and password from reduxForm
         const { handleSubmit, fields: { email, password }} = this.props;
@@ -21,13 +28,16 @@ class Signin extends Component {
             <label>Password:</label>
             <input {...password} type="password" className="form-control" />
             </fieldset>
+            {this.renderAlert()}
             <button action="submit" className="btn btn-primary">Sign in</button>
         </form>
         );
     }
 }
-
+function mapStateToProps(state) {
+    return { errorMessage: state.auth.error };
+}
 export default reduxForm({
     form: 'signin',
     fields: ['email', 'password']
-}, null, actions)(Signin);
+}, mapStateToProps, actions)(Signin);
